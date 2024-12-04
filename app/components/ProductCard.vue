f8d78df15b27901042ae11b4ff92a1ee1cc1b2df
Ikechukwu <31142429+anudeike@users.noreply.github.com>
2024-12-04 10:31:27 -0500

    added hover overlay

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -1,20 +1,15 @@
 <template>
     <v-hover v-slot="{ isHovering, props }" open-delay="200" close-delay="200">
-        <v-card variant="flat" color="transparent" v-bind="props">
-            <v-img :src="product.imageLink" aspect-ratio="1.5" cover>
-                <!-- transition here -->
-                <v-expand-transition>
-                    <div v-if="isHovering"
-                        class="d-flex transition-fast-in-fast-out bg-orange-darken-2 v-card--reveal text-h2"
-                        style="height: 100%;">
-                        $14.99
-                    </div>
-                </v-expand-transition>
+        <v-card variant="flat" color="transparent" v-bind="props" elevation="0">
+            <v-img :src="product.imageLink" aspect-ratio="1.5" :class="{ 'on-hover': isHovering }" cover>
             </v-img>
 
-            <v-card-title>{{ product.name }}</v-card-title>
-            <v-card-subtitle>${{ product.price }}</v-card-subtitle>
-            <v-card-text>{{ product.description }}</v-card-text>
+            <v-overlay :model-value="!!isHovering" class="align-center justify-center" scrim="#036358" contained>
+                <v-card-title color="white">{{ product.name }}</v-card-title>
+            </v-overlay>
+
+            <!-- <v-card-subtitle>${{ product.price }}</v-card-subtitle>
+            <v-card-text>{{ product.description }}</v-card-text> -->
         </v-card>
     </v-hover>
 </template>
@@ -31,14 +26,6 @@ export default {
 </script>
 
 <style scoped>
-.v-card {
-    transition: opacity .4s ease-in-out;
-}
-
-.v-card:not(.on-hover) {
-    opacity: 0.6;
-}
-
 .show-btns {
     color: rgba(255, 255, 255, 1) !important;
 }
